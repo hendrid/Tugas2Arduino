82651effa86b6f068c86babcb34980ae8d1a8c03
Hendri <46194260+hendrid@users.noreply.github.com>
2020-05-12 11:11:59 +0700

    Update 4.ino

--- 4.ino
+++ 4.ino
@@ -1,11 +1,12 @@
 #define echoPin 6 
 #define trigPin 7
-#define buzzerPin 4
+#define buzzerPin 5
 long duration,distance;   
 void setup() {
   // put your setup code here, to run once:
   pinMode(echoPin,INPUT);
   pinMode(trigPin,OUTPUT);
+  pinMode(buzzerPin,OUTPUT);
   Serial.begin(9600);
 }
 
@@ -18,8 +19,7 @@ void loop() {
   digitalWrite(trigPin, LOW); 
   duration = pulseIn(echoPin, HIGH); 
   distance = duration/58.2;
-  int f=1098-distance;
-  tone(buzzerPin,f);
+  analogWrite(buzzerPin,(1118-distance)/4.38);
   Serial.print("Jarak : ");
   Serial.print(distance);
   Serial.println(" cm");
