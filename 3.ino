33566c82854ade06e9d33558d508ba0ec64f95e3
Hendri <46194260+hendrid@users.noreply.github.com>
2020-05-31 19:40:54 +0700

    Update 3.ino

--- 3.ino
+++ 3.ino
@@ -1,29 +1,39 @@
-int pinLdr=A5;
-int ldr=0;
-int motorPinr=8;
-int motorPinl=7;
-int speed=0;
-void setup() { 
-   pinMode(motorPinr,OUTPUT);
-   pinMode(motorPinl,OUTPUT);  
-   Serial.begin(9600); 
-} 
-void loop() { 
-  ldr=analogRead(pinLdr);
+int pinLdr = A5;
+int ldr = 0;
+int en1 = 5, en2 = 6;
+int in[] = {9, 8, 4, 3};
+int speed = 0;
+void setup() {
+  for (int i = 0; i < 4; i++) {
+    pinMode(in[i], OUTPUT);
+  }
+  Serial.begin(9600);
+}
+void loop() {
+  ldr = analogRead(pinLdr);
   Serial.print("Nilai LDR = ");
   Serial.print(ldr);
-  speed=map(ldr,0,1023,0,255);
-      if(ldr<500){
-        analogWrite(motorPinl, speed);
-        analogWrite(motorPinr, 0); 
-        Serial.print("\t <kiri< Kecepatan Motor = ");
-        Serial.println(speed);
-      }
-      else{
-        analogWrite(motorPinl, 0); 
-        analogWrite(motorPinr, speed); 
-        Serial.print("\t >kanan> Kecepatan Motor = ");
-        Serial.println(speed);
-      }
-  delay(100);
+  speed = map(ldr, 0, 1023, 0, 255);
+  if (ldr < 500) {
+    analogWrite(en1, speed);
+    analogWrite(en2, speed);
+    digitalWrite(in[0], LOW);
+    digitalWrite(in[1], HIGH);
+    digitalWrite(in[2], LOW);
+    digitalWrite(in[3], HIGH);
+    Serial.print("\t <kiri< Kecepatan Motor = ");
+    Serial.println(speed);
+    delay(5);
+  }
+  else {
+    analogWrite(en1, speed);
+    analogWrite(en2, speed);
+    digitalWrite(in[0], HIGH);
+    digitalWrite(in[1], LOW);
+    digitalWrite(in[2], HIGH);
+    digitalWrite(in[3], LOW);
+    Serial.print("\t >kanan> Kecepatan Motor = ");
+    Serial.println(speed);
+    delay(5);
+  }
 }
